afafadf98cce2aa3231da9e6dc714fe002cd0acf
natiproooo <nataliabaran1204@gmail.com>
2026-04-27 14:58:36 +0000

    vector fuerza

--- Práctica 12/VectorFuerzaLecPR.c
+++ Práctica 12/VectorFuerzaLecPR.c
@@ -4,26 +4,60 @@
 
 #define MAX 100
 
-typedef float VectorFuerzas[MAX];
+typedef float tipo_vector[MAX];
+
+typedef struct {
+    int tam;            
+    tipo_vector vector; 
+} tipo_fuerzas;
+
+
+void leerFuerzas(tipo_fuerzas *f) {
+    float aux = 0; 
+    f->tam = 0;    
+
+    printf("Introduce las fuerzas (un numero negativo para terminar):\n");
+
+    
+    printf("Fuerza %d: ", f->tam + 1);
+    scanf("%f", &aux);
+
+    
+    while (aux >= 0 && f->tam < MAX) {
+        
+        f->vector[f->tam] = aux;
+        f->tam++;
+
+        
+        if (f->tam < MAX) {
+            printf("Fuerza %d: ", f->tam + 1);
+            scanf("%f", &aux);
+        }
+    }
+}
 
 
 int main() {
 
-    int numSecciones = 6;
-    float fuerzaTotal;
-    VectorFuerzas fuerzas = {1000, 1500, 800, 1200, 2000, 22};
+    tipo_fuerzas mis_fuerzas;
+    float fuerzaTotal = 0;
+    float promedio = 0;
 
-    for (int i = 0; i < numSecciones; ++i) {
-        fuerzaTotal += fuerzas[i];
+    leerFuerzas(&mis_fuerzas);
+
+    if (mis_fuerzas.tam == 0)
+    {
+        printf("No hay valores, porfavor introduzca unos valores que sean correctos");
+
+    }else{
+        
+        for (int i = 0; i < mis_fuerzas.tam; ++i) {
+        fuerzaTotal += mis_fuerzas.vector[i];
+        }
+        promedio = fuerzaTotal / mis_fuerzas.tam;
+    
+        printf("\n La fuerza total en el puente es de %.2f, el promedio es de %.2f y el numero de secciones es %d", fuerzaTotal, promedio, mis_fuerzas.tam);
     }
-
-
-    printf("Fuerzas en las secciones del puente:\n");
-    for (int i = 0; i < numSecciones; ++i) {
-        printf("Sección %d: %f Newtons\n", i + 1, fuerzas[i]);
-    }
-
-    printf("\nFuerza total en el puente: %.2f Newtons\n", fuerzaTotal);
 
     return 0;
 }
